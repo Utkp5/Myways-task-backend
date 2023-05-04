4c82b0a97069e15faef95b9b9c77f353fad0f720
Utkp5 <utkp200@gmail.com>
2023-05-04 20:09:56 +0530

    update  chng

--- Service/dbConfig.js
+++ Service/dbConfig.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const dbconnect = async() => {
     try {
         
-        await mongoose.connect('mongodb://127.0.0.1:27017/MywaysTask');
+        await mongoose.connect('mongodb://localhost:27017/MywaysTask');
         console.log('Mongodb connected successfully');
 
     } catch (error) {
